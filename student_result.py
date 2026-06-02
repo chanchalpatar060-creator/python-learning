d8c4da3189a349018ffdf9bcf3fb56b64f3a1305
chanchalpatar060-creator <chanchalpatar060@gmail.com>
2026-06-03 00:42:50 +0530

    Create student_result.py

---
+++ student_result.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter Marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
+
+print("Your Grade is:", grade)
